3863ad06069475970f3f3f2a6506ab5e261fd6df
奥田啓皓 <>
2021-11-24 20:12:33 +0900

    webpack.config.jsの変更

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist/index.html'),
     publicPath: './',
     filename: 'build.js'
   },
